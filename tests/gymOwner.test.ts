7f3bbf95b54555fdf54959b36b64b053f28d55c2
ysherbini <ysherbini@identity-solutions.org>
2024-01-04 20:55:35 +0200

    ignore tests folder

--- tests/gymOwner.test.ts
+++ tests/gymOwner.test.ts
@@ -1,39 +1,19 @@
 import 'reflect-metadata';
 import request from 'supertest';
-
-import '../dist/config/index';
-import jwt from 'jsonwebtoken';
-import mongoose from 'mongoose';
-
-import GymOwner from '../src/models/gymOwner';
-
 import express from 'express';
 
+import '../dist/config/index';
+import GymOwner from '../src/models/gymOwner';
+import { gymOwnerOneId, gymOwnerOne, setupDatabase } from './db/db'
 import { App } from '../src/app';
+
 let app: express.Application;
 
 beforeAll(async () => {
     app = await new App().setup();
 });
 
-const userOneId = new mongoose.Types.ObjectId();
-const userOne = {
-    _id: userOneId,
-    name: 'Yahya ihab',
-    image: "",
-    email: 'yahyaihab@gmail.com',
-    password: 'Password',
-    tokens: [
-        {
-            token: jwt.sign({ _id: userOneId }, `${process.env.JWT_SECRET}`),
-        },
-    ],
-};
-
-beforeEach(async () => {
-    await GymOwner.deleteMany();
-    await new GymOwner(userOne).save();
-});
+beforeEach(setupDatabase);
 
 test('Should signup a new user', async () => {
     const response = await request(app)
@@ -51,11 +31,11 @@ test('Should login existing user', async () => {
     const response = await request(app)
         .post('/gymOwners/login')
         .send({
-            email: userOne.email,
-            password: userOne.password,
+            email: gymOwnerOne.email,
+            password: gymOwnerOne.password,
         })
         .expect(200);
-    const gymOwner = await GymOwner.findById(userOneId)
+    const gymOwner = await GymOwner.findById(gymOwnerOneId)
     expect(response.body.token).not.toBeNull();
     expect(response.body.token).toBe(gymOwner?.tokens[1].token)
 });
@@ -71,7 +51,7 @@ test('Should not login existing user', async () => {
 });
 
 test('Should get profile for user', async () => {
-    await request(app).get('/gymOwners/profile').set('Authorization', `Bearer ${userOne.tokens[0].token}`).send().expect(200);
+    await request(app).get('/gymOwners/profile').set('Authorization', `Bearer ${gymOwnerOne.tokens[0].token}`).send().expect(200);
 });
 
 test('Should not get profile for unauthenticated user', async () => {
@@ -79,8 +59,8 @@ test('Should not get profile for unauthenticated user', async () => {
 });
 
 test('Should delete account for user', async () => {
-    await request(app).delete('/gymOwners/profile').set('Authorization', `Bearer ${userOne.tokens[0].token}`).send().expect(200);
-    const gymOwner = await GymOwner.findById(userOneId)
+    await request(app).delete('/gymOwners/profile').set('Authorization', `Bearer ${gymOwnerOne.tokens[0].token}`).send().expect(200);
+    const gymOwner = await GymOwner.findById(gymOwnerOneId)
     expect(gymOwner).toBeNull()
 });
 
@@ -91,17 +71,19 @@ test('Should not delete account for unauthenticated user', async () => {
 test('Should update valid user fields', async () => {
     const response = await request(app)
         .patch('/gymOwners/profile')
-        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .set('Authorization', `Bearer ${gymOwnerOne.tokens[0].token}`)
         .send({
             name: "moh"
         })
         .expect(200);
+    const gymOwner = await GymOwner.findById(gymOwnerOneId)
+    expect(gymOwner?.name).toEqual('moh')
 });
 
 test('Should not update invalid user fields', async () => {
     await request(app)
         .patch('/gymOwners/profile')
-        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .set('Authorization', `Bearer ${gymOwnerOne.tokens[0].token}`)
         .send({
             name: ""
         })
